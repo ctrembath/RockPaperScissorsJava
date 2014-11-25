f7badb7bc12f0549f395a1523d7beb79c2eada3b
ctrembath <claretrembath@ymail.com>
2014-11-25 15:26:22 +0000

    started Java Rock Paper Scissors

---
+++ src/RockPaperScissors.js
@@ -0,0 +1,14 @@
+function Player(name) {
+  this.name = name;
+}
+
+Player.prototype.picks = function(){
+  }
+
+function Game() {
+
+  }
+
+Game.prototype.winner = function() {
+    return player1;
+}
